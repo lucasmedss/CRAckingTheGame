97752ed5e8aa16e1f7c911d198ca5d74a0070686
Jônatas Tavares dos Santos <jonatas.santos@ccc.ufcg.edu.br>
2023-05-10 08:47:30 -0300

    Tabuleiro e menu principal

--- Funcional/app/Main.hs
+++ Funcional/app/Main.hs
@@ -1,4 +1,51 @@
 module Main where
 
+import Data.Char (toUpper)
+import GHC.IO.Handle (hClose)
+import System.Console.ANSI
+import System.IO
+import System.Random
+import Tabuleiro (exibirTabuleiro, getTabuleiro, modificarTabuleiro)
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  menu getTabuleiro
+
+menu :: [String] -> IO ()
+menu tabuleiro = do
+  clearScreen
+  setCursorPosition 0 0
+  putStrLn "Seja bem-vindo ao CRAcking the Game!"
+  arquivo <- openFile "./database/menu.txt" ReadMode
+  conteudo <- hGetContents arquivo
+  putStrLn conteudo
+  hClose arquivo
+  opcao <- getLine
+  case map toUpper opcao of
+    "J" -> do
+      putStrLn "Jogar"
+      jogar tabuleiro 0
+    "S" -> do
+      putStrLn "Obrigado por jogar CRAcking the Game\nAté mais!"
+      return ()
+    _ -> do
+      putStrLn "Opção inválida"
+      menu tabuleiro
+
+jogar :: [String] -> Int -> IO ()
+jogar tabuleiro atual = do
+  dado <- randomRIO (1, 6 :: Int)
+  exibirTabuleiro tabuleiro
+  let novaPosicao = atual + dado
+  if novaPosicao >= 33
+    then do
+      let novoTabuleiro = modificarTabuleiro tabuleiro "CC"
+      exibirTabuleiro novoTabuleiro
+      putStrLn "Você chegou ao final do tabuleiro! Parabéns!\nPressione ENTER para voltar para o menu principal."
+      esperar <- getLine
+      main
+    else do
+      let novoTabuleiro = modificarTabuleiro tabuleiro (show novaPosicao)
+      putStrLn ("Você tirou: " ++ show dado ++ "\nPressione ENTER para continuar")
+      esperar <- getLine
+      jogar novoTabuleiro novaPosicao
---
+++ Funcional/app/Tabuleiro.hs
@@ -0,0 +1,55 @@
+module Tabuleiro where
+
+import Data.List (intercalate, isInfixOf, isPrefixOf)
+import System.Console.ANSI
+import System.IO
+import System.IO.Unsafe (unsafePerformIO)
+import Text.Printf (printf)
+
+getTabuleiro :: [String]
+{-# NOINLINE getTabuleiro #-}
+getTabuleiro = do
+  let tabuleiro = unsafePerformIO (readFile "./database/tabuleiro.txt")
+  lines tabuleiro
+
+exibirTabuleiro :: [String] -> IO ()
+exibirTabuleiro tabuleiro = do
+  clearScreen
+  setCursorPosition 0 0
+  putStrLn (unlines tabuleiro)
+
+getLinha :: [String] -> String -> Int -> Int
+getLinha [] _ _ = -1
+getLinha (x : xs) casa index
+  | casa `isInfixOf` x = index
+  | otherwise = getLinha xs casa (index + 1)
+
+getColuna :: String -> String -> Int
+getColuna linha casa
+  | casa `isPrefixOf` linha = 0
+  | otherwise = case getColuna (drop 1 linha) casa of
+      index
+        | index /= -1 -> index + 1
+        | otherwise -> -1
+
+getPosicao :: [String] -> String -> (Int, Int)
+getPosicao tabuleiro casa = do
+  let linha = getLinha tabuleiro casa 0
+  let coluna = getColuna (tabuleiro !! linha) casa
+  (linha, coluna)
+
+modificarTabuleiro :: [String] -> String -> [String]
+modificarTabuleiro tabuleiro proximaPosicao = do
+  let atual = getPosicao tabuleiro "X"
+  let linhaTabuleiro = tabuleiro !! fst atual
+  let novaLinha = replaceChar (snd atual) " " linhaTabuleiro
+  let novoTabuleiro = intercalate "\n" (take (fst atual) tabuleiro ++ [novaLinha] ++ drop (fst atual + 1) tabuleiro)
+
+  let proximoAtual = getPosicao (lines novoTabuleiro) (printf "%02s" proximaPosicao)
+  let proximaLinhaTabuleiro = lines novoTabuleiro !! (fst proximoAtual - 1)
+  let proximaNovaLinha = replaceChar (snd proximoAtual + 1) "X" proximaLinhaTabuleiro
+  let proximoNovoTabuleiro = intercalate "\n" (take (fst proximoAtual - 1) (lines novoTabuleiro) ++ [proximaNovaLinha] ++ drop (fst proximoAtual) (lines novoTabuleiro))
+  return proximoNovoTabuleiro
+
+replaceChar :: Int -> String -> String -> String
+replaceChar i c s = take i s ++ c ++ drop (i + 1) s
